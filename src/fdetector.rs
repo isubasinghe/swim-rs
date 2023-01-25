81a5d3b4725cc0b6ffab732b846583e61610209d
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2023-01-25 22:13:21 +1100

    added stuff

--- src/fdetector.rs
+++ src/fdetector.rs
@@ -1,3 +1,5 @@
+use rand::rngs::ThreadRng;
+use rand::Rng;
 use std::collections::HashMap;
 
 use protocol::Protocol;
@@ -19,10 +21,27 @@ pub struct SwimFailureDetector {
     id: Vec<u8>,
     nodes: Vec<Vec<u8>>,
     addrs: Vec<Addr>,
+    rng: ThreadRng,
+    period: u32,
+    failure_group_sz: u32,
 }
 
 impl SwimFailureDetector {
-    pub fn run_round(&self) {}
+    pub fn new() {
+        let mut rng = rand::thread_rng();
+    }
+
+    pub fn run_round(&mut self) {
+        if self.nodes.len() < 1 {
+            return;
+        }
+
+        let index = self.rng.gen_range(0..self.nodes.len());
+
+        for _ in 0..self.failure_group_sz {
+            let index = self.rng.gen_range(0..self.nodes.len());
+        }
+    }
 
     pub fn add_node(&self, node_id: Vec<u8>, addr: Addr) {}
 }
